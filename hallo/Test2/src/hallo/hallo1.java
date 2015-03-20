7d4ac2841d7de901e2431be68ab73a3fa949b070
Florian Hallay <florian.hallay@yahoo.de>
2015-03-20 23:20:18 +0100

    hey noch so ein test ding

--- hallo/Test2/src/hallo/hallo1.java
+++ hallo/Test2/src/hallo/hallo1.java
@@ -3,5 +3,10 @@ package hallo;
 public class hallo1 {
 	public static void main(String args){
 		System.out.println("Hallo");
+		System.out.println(new hallo1("Hallo"));
+	}
+	String hallo;
+	public hallo1(String hallo){
+		this.hallo=hallo;
 	}
 }
