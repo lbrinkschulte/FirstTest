afc48d8349759b7a8f7434fd52b48b7a37693506
Florian Hallay <florian.hallay@yahoo.de>
2015-03-20 23:56:48 +0100

    Direct Eclipse EGit Push

--- hallo/Test2/src/hallo/hallo1.java
+++ hallo/Test2/src/hallo/hallo1.java
@@ -1,14 +1,20 @@
 package hallo;
 
 public class hallo1 {
-	public static void main(String args){
+	public static void main(String[] args){
 		System.out.println("Hallo");
-		System.out.println(new hallo1("Hallo"));
+		System.out.println(new hallo1("Hallo")+" Welt");
 		System.out.println("hallo");
 		System.out.println("hallo");
 	}
 	String hallo;
 	public hallo1(String hallo){
 		this.hallo=hallo;
+	}
+	
+	@Override
+	public String toString(){
+		return hallo;
+		
 	}
 }
