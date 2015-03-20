ba4aaba5ef238ed72932f7a8812f1cb0729718c8
Florian Hallay <florian.hallay@yahoo.de>
2015-03-20 23:41:36 +0100

    hallo

--- hallo/Test2/src/hallo/hallo1.java
+++ hallo/Test2/src/hallo/hallo1.java
@@ -4,6 +4,7 @@ public class hallo1 {
 	public static void main(String args){
 		System.out.println("Hallo");
 		System.out.println(new hallo1("Hallo"));
+		System.out.println("hallo");
 	}
 	String hallo;
 	public hallo1(String hallo){
